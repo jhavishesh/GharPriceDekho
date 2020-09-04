cffc000824bd7be069a43098546eb729128e9ec7
Vishesh Jha <61939693+jhavishesh@users.noreply.github.com>
2020-09-04 20:18:46 +0530

    Update predict.php

--- predict.php
+++ predict.php
@@ -1,6 +1,6 @@
 <?php
 //GET Parameters
-
+// instance ip address 18.222.253.208
 
 $lotsize = $_GET["lotsize"];
 $bathrms = $_GET["bathrms"];
@@ -14,9 +14,15 @@ $garagepl = $_GET["garagepl"];
 $prefarea = $_GET["prefarea"];
 
 
+//this is way to pass argument in php through which model will get execute.
+
+//http://here is instance ip address which is now(18.222.253.208)/predict.php?lotsize=2000&driveway=1&recroom=1&fullbase=1&gashw=1&airco=1&garagepl=1&prefarea=1
+
+
 //Execute the model
-system("/usr/anaconda/bin/python3 vishesh_model.py ".$lotsize." ".$bathrms." ".$stories." ".$driveway." ".$recroom." ".$fullbase." ".$gashw." ".$airco." ".$garagepl." ".$prefarea." 2>&1");
+system("/usr/anaconda/bin/python3 home.py ".$lotsize." ".$bathrms." ".$stories." ".$driveway." ".$recroom." ".$fullbase." ".$gashw." ".$airco." ".$garagepl." ".$prefarea." 2>&1");
 
 
 
 ?>
+
