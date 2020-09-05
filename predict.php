c0a7a019a3f52ef4ccc44ec15f176558a8c89191
Vishesh Jha <61939693+jhavishesh@users.noreply.github.com>
2020-09-05 15:13:40 +0530

    Update predict.php

--- predict.php
+++ predict.php
@@ -2,16 +2,6 @@
 //GET Parameters
 
 
-$lotsize = 0;
-$bathrms = 0;
-$stories = 0;
-$driveway = 0;
-$recroom = 0;
-$fullbase = 0;
-$gashw = 0;
-$airco =0;
-$garagepl = 0;
-$prefarea =0; 
 
 $lotsize = $_GET["lotsize"];
 $bathrms = $_GET["bathrms"];
@@ -25,6 +15,16 @@ $garagepl = $_GET["garagepl"];
 $prefarea = $_GET["prefarea"];
 
 
+$lotsize = 0;
+$bathrms = 0;
+$stories = 0;
+$driveway = 0;
+$recroom = 0;
+$fullbase = 0;
+$gashw = 0;
+$airco =0;
+$garagepl = 0;
+$prefarea =0; 
 //Execute the model
 system("/usr/anaconda/bin/python3 home.py ".$lotsize." ".$bathrms." ".$stories." ".$driveway." ".$recroom." ".$fullbase." ".$gashw." ".$airco." ".$garagepl." ".$prefarea." 2>&1");
 
