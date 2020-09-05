7c073dc5155ab17ec8a48f0d7871a0ea4c493ea5
Vishesh Jha <61939693+jhavishesh@users.noreply.github.com>
2020-09-05 15:24:31 +0530

    Update predict.php

--- predict.php
+++ predict.php
@@ -15,16 +15,6 @@ $garagepl = $_GET["garagepl"];
 $prefarea = $_GET["prefarea"];
 
 
-$lotsize = 0;
-$bathrms = 0;
-$stories = 0;
-$driveway = 0;
-$recroom = 0;
-$fullbase = 0;
-$gashw = 0;
-$airco =0;
-$garagepl = 0;
-$prefarea =0; 
 //Execute the model
 system("/usr/anaconda/bin/python3 home.py ".$lotsize." ".$bathrms." ".$stories." ".$driveway." ".$recroom." ".$fullbase." ".$gashw." ".$airco." ".$garagepl." ".$prefarea." 2>&1");
 
